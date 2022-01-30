a8ce748a81bc6586b9d72ff4b0d15601269e239f
Kristina <d.kristina27@yandex.ru>
2022-01-30 23:07:14 +0400

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  for(let j = 0; j < str.length; j++) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (str[j] == bracketsConfig[i][0] && str[j+1] == bracketsConfig[i][1]) {
+          str.splice(j, 2);
+          j = j - (j+1);
+        }
+      }
+    }
+  if (str.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
